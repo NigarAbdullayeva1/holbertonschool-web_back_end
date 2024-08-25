1635b90477efbc4a29e7f08dc09defa46deb11ab
NigarABdullayeva1 <nabdulayevash7@gmail.com>
2024-08-25 14:04:09 +0000

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
-""" 12. Log stats """
-
-
 from pymongo import MongoClient
 
-
-if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
-
+def log_stats():
+    # Connect to the MongoDB server
+    client = MongoClient('mongodb://localhost:27017/')
     db = client.logs
-
     collection = db.nginx
 
-    count = collection.count_documents({})
+    # Total number of logs
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
 
-    print("{} logs".format(count))
-    print("Methods:")
-
+    # Count each method
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
+    print("Methods:")
     for method in methods:
-        count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
 
-    countGet = collection.count_documents({"method": "GET", "path": "/status"})
+    # Count for method=GET and path=/status
+    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
 
-    print("{} status check".format(countGet))                                                          
+if __name__ == "__main__":
+    log_stats()
+                                    
